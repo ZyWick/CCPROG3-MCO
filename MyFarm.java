3025e5d360d1320031827d5f388dd34d7fe31564
Joshua Corpuz <104405536+ZyWick@users.noreply.github.com>
2022-10-31 16:09:33 +0800

    Update MyFarm.java

--- MyFarm.java
+++ MyFarm.java
@@ -1,19 +1,83 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 
 public class MyFarm {
-    private ArrayList<Tile> lot;
+    private ArrayList<Tile> lot = new ArrayList<Tile>();
 
     public MyFarm() {
-        int x, y;
+        int x;
 
-        for(x = 1; x <=5; x++)
-            for(y = 1; y <= 10; y++) {
+        for(x = 0; x <50; x++)
                 //if indicated
                 lot.add(new Tile(false));
                 //lot.add(new Tile(true));
+            
+    }
+
+    public char whatToPrint(Tile tile) {
+        if (tile.isPlowed()) {
+            if (tile.getSeeds() != null) {
+                int day = tile.getDay();
+                int harvestTime = tile.getSeeds().getHarvestTime();
+
+                if (day == 1)
+                    return ',';
+                if (day <= harvestTime/3)
+                    return 's';
+                if (day <= harvestTime/1.5)
+                    return 'S'; 
+                if (day == harvestTime)
+                    return '$';            
             }
+            return '#';
+        } else if (tile.isRock()) {
+            return '*';
+        } else if (tile.isWithered()) {
+            return 'X';
+        } else {
+            return '=';
+        }
+    }
+
+    public void display () {
+        int x, y, tileIndex = 0;
+
+        for(x = 0; x < 5; x++) {
+            for(y = 0; y < 10; y++) 
+                System.out.print("* - - - - - ");
+            System.out.println('*');
+
+            for(y = 0; y< 10; y++)
+                System.out.print("|           ");
+            System.out.println('|');
+                
+            for(y = 0; y < 10; y++) {
+                System.out.print("|     ");
+                System.out.print(whatToPrint(lot.get(tileIndex)));
+                System.out.print("     ");
+                tileIndex = tileIndex + 1;
+            }
+            System.out.println('|');
+
+            for(y = 0; y< 10; y++)
+                System.out.print("|           ");
+            System.out.println('|');
+        }        
+        for(y = 0; y < 10; y++) 
+            System.out.print("* - - - - - ");
+        System.out.println('*');
+    }
+    
+    public int getTileIndex(Scanner sc) {
+        int x, y, tileIndex;
+
+        System.out.print("input tile coordinates: ");
+        x = sc.nextInt();
+        y = sc.nextInt();
+        tileIndex = (x - 1) * 10 + (y - 1) ;
+        return tileIndex;
     }
 
     public void plowTile(int index) {
