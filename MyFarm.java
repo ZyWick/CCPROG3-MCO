d66d2650e96061a7086aad12d97ab99807c5c096
Joshua Corpuz <104405536+ZyWick@users.noreply.github.com>
2022-11-04 20:22:12 +0800

    Update MyFarm.java

--- MyFarm.java
+++ MyFarm.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 
@@ -9,7 +8,7 @@ public class MyFarm {
     public MyFarm() {
         int x;
 
-        for(x = 0; x <50; x++)
+        for(x = 0; x <1; x++)
                 //if indicated
                 lot.add(new Tile(false));
                 //lot.add(new Tile(true));
@@ -29,31 +28,32 @@ public class MyFarm {
                 if (day <= harvestTime/1.5)
                     return 'S'; 
                 if (day == harvestTime)
-                    return '$';            
+                    return '$'; 
+                if (tile.isWithered() || day > harvestTime)
+                return 'X';        
             }
             return '#';
         } else if (tile.isRock()) {
             return '*';
-        } else if (tile.isWithered()) {
-            return 'X';
         } else {
             return '=';
         }
     }
 
     public void display () {
-        int x, y, tileIndex = 0;
+        int x, y, row = 1, column = 1, tileIndex = 0;
 
-        for(x = 0; x < 5; x++) {
-            for(y = 0; y < 10; y++) 
+
+        for(x = 0; x < row; x++) {
+            for(y = 0; y < column; y++) 
                 System.out.print("* - - - - - ");
             System.out.println('*');
 
-            for(y = 0; y< 10; y++)
+            for(y = 0; y < column; y++)
                 System.out.print("|           ");
             System.out.println('|');
                 
-            for(y = 0; y < 10; y++) {
+            for(y = 0; y < column; y++) {
                 System.out.print("|     ");
                 System.out.print(whatToPrint(lot.get(tileIndex)));
                 System.out.print("     ");
@@ -61,60 +61,48 @@ public class MyFarm {
             }
             System.out.println('|');
 
-            for(y = 0; y< 10; y++)
+            for(y = 0; y < column; y++)
                 System.out.print("|           ");
             System.out.println('|');
         }        
-        for(y = 0; y < 10; y++) 
+        for(y = 0; y < column; y++) 
             System.out.print("* - - - - - ");
         System.out.println('*');
     }
     
-    public int getTileIndex(Scanner sc) {
-        int x, y, tileIndex;
-
-        System.out.print("input tile coordinates: ");
-        x = sc.nextInt();
-        y = sc.nextInt();
-        tileIndex = (x - 1) * 10 + (y - 1) ;
-        return tileIndex;
+    public void plowTile(Tile TheTile) {
+        TheTile.setPlowed(true);
     }
 
-    public void plowTile(int index) {
-        lot.get(index).setPlowed(true);
+    public void waterTile(Tile TheTile) {
+        TheTile.addWaterTimes();
     }
 
-    public void waterTile(int index) {
-        lot.get(index).addWaterTimes();
+    public void fertilizeTile(Tile TheTile) {
+        TheTile.addFertilizerTimes();   
     }
 
-    public void fertilizeTile(int index) {
-        lot.get(index).addFertilizerTimes();   
+    public void removeRock(Tile TheTile) {
+        TheTile.setRock(false);  
     }
 
-    public void removeRock(int index) {
-        lot.get(index).setRock(false);  
-    }
+    public void removeWithered (Tile TheTile) {
 
-    public void removeWithered (int index) {
-        Tile tile = lot.get(index);
-
-        if (tile.isWithered()) {
-            tile = new Tile();
-        } else if (tile.isPlowed() && tile.getSeeds() != null) {
-            tile = new Tile();
-        } else if (tile.isPlowed() == false || tile.isRock()) {
+        if (TheTile.isWithered()) {
+            TheTile = new Tile();
+        } else if (TheTile.isPlowed() && TheTile.getSeeds() != null) {
+            TheTile = new Tile();
+        } else if (TheTile.isPlowed() == false || TheTile.isRock()) {
 
         }
 
     }
 
-    public void plantCrop(int index, FarmSeeds seed) {
-        lot.get(index).setSeeds(seed);
+    public void plantCrop(Tile TheTile, FarmSeeds selectedSeed) {
+        TheTile.setSeeds(selectedSeed);
    } 
 
-    public int getProductsProduced(int tileIndex) {
-        Tile TheTile = lot.get(tileIndex);
+    public int getProductsProduced(Tile TheTile) {
         FarmSeeds TheSeed = TheTile.getSeeds();
         int productsProduced;
         
@@ -124,13 +112,18 @@ public class MyFarm {
         return productsProduced;
     }
 
-    public void addDay() {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
-        for (Tile tile : lot)
-            if(tile.getSeeds() != null)
+    public void ageLot() {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+        for (Tile tile : lot) 
+            if(tile.getSeeds() != null) {
                 tile.addDay();
+                if (tile.getDay() > tile.getSeeds().getHarvestTime())
+                    tile.setWithered(true);
+            }
     }
 
     public ArrayList<Tile> getLot() {
         return this.lot;
     }
+
+    
 }
