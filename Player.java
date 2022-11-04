f3c7f5c630d943899b858d3988c6cbb238220f19
Joshua Corpuz <104405536+ZyWick@users.noreply.github.com>
2022-11-04 20:20:35 +0800

    Update Player.java

--- Player.java
+++ Player.java
@@ -3,63 +3,97 @@ public class Player {
     private int objectCoins = 100;
     private double exp = 0.0;
     private int level = 1;
-    private FarmSystem god = new FarmSystem();
-    private FarmerType type = god.getType().get(0);
+    private FarmSystem game = new FarmSystem();
+    private FarmerType type = game.getType().get(0);
 
     public Player() {
     }
-
+    
     public void levelUp(double exp) {
         if (this.level < exp / 100)
             this.level = (int)(exp / 100);
     }
 
-    public void useTool (MyFarm farm, int toolIndex, int tileIndex) {
-        FarmTools tool = god.getTools().get(toolIndex);
-        if (objectCoins >= tool.getUsageCost()) {
+    public void displayPlayerStats() {
+        System.out.print("ObjectCoins: " + this.objectCoins);
+        System.out.print(" exp: " + this.exp);
+        System.out.print(" level: " + this.level);
+        if (game.canRegisterUp(this.type, this.level))
+            System.out.println("can register superior farmer type");
+        else 
+            System.out.println();
+    }
+
+    public void useTool (MyFarm farm, FarmTools selectedTool, Tile TheTile) {
+        if (game.canUseTool(selectedTool, TheTile, this.objectCoins)) {
             
-            switch (toolIndex) {
-                case 0: farm.plowTile(tileIndex); break;
-                case 1: farm.waterTile(tileIndex); break;
-                case 2: farm.fertilizeTile(tileIndex); break;
-                case 3: farm.removeRock(tileIndex); break;
-                case 4: farm.removeWithered(tileIndex); break;
+            switch (game.getTools().indexOf(selectedTool)) {
+                case 0: farm.plowTile(TheTile); break;
+                case 1: farm.waterTile(TheTile); break;
+                case 2: farm.fertilizeTile(TheTile); break;
+                case 3: farm.removeRock(TheTile); break;
+                case 4: farm.removeWithered(TheTile); break;
             }
             
-            objectCoins -= tool.getUsageCost();
-            exp += tool.getExpYield();
+            objectCoins -= selectedTool.getUsageCost();
+            exp += selectedTool.getExpYield();
             levelUp(exp);            
         }
+        else
+            game.throwToolError(selectedTool);
     }
 
-    public void plantCrop (MyFarm farm, int tileIndex, int seedIndex) {
-        farm.plantCrop(tileIndex, god.getSeeds().get(seedIndex));
+    public void plantCrop (MyFarm farm, Tile TheTile, FarmSeeds selectedSeed) {
+        if (game.canUseSeed(selectedSeed, this.objectCoins))        
+            farm.plantCrop(TheTile, selectedSeed);
+        else
+            game.throwSeedError();
     }
 
-    public void harvestCrop (MyFarm farm, int tileIndex) {
+    public void harvestCrop (MyFarm farm, Tile TheTile) {
+        if (game.canHarvest(TheTile)) {
+            FarmSeeds TheSeed = TheTile.getSeeds();
+            int productsProduced = farm.getProductsProduced(TheTile);
+            double harvestTotal, waterBonus, fertilizerBonus;      
+            
+            harvestTotal = productsProduced * (TheSeed.getSellingPrice() + type.getBonusEarning());
+            waterBonus = harvestTotal * 0.2 * (TheTile.getWaterTimes() - 1);
+            fertilizerBonus = harvestTotal * 0.5 * TheTile.getFertilizerTimes();
+            harvestTotal = harvestTotal + waterBonus + fertilizerBonus;
 
-        Tile TheTile = farm.getLot().get(tileIndex);
-        FarmSeeds TheSeed = TheTile.getSeeds();
-        int productsProduced = farm.getProductsProduced(tileIndex);
-        double harvestTotal, waterBonus, fertilizerBonus;      
-        
-        harvestTotal = productsProduced * (TheSeed.getSellingPrice() + type.getBonusEarning());
-        waterBonus = harvestTotal * 0.2 * (TheTile.getWaterTimes() - 1);
-        fertilizerBonus = harvestTotal * 0.5 * TheTile.getFertilizerTimes();
-        harvestTotal = harvestTotal + waterBonus + fertilizerBonus;
+            this.objectCoins += harvestTotal;
+            this.exp += TheSeed.getExpYield();
+            levelUp(exp); 
+        }
+        else
+            game.throwHarvestError(TheTile);
+    }
 
-        this.objectCoins += harvestTotal;
-        this.exp += TheSeed.getExpYield();
-        levelUp(exp); 
+    public void RegisterUp() {
+        if (game.canRegisterUp(this.type, this.level))
+            this.type = game.getType().get(game.getType().indexOf(this.type) + 1);
+        else
+            game.throwRegisterError();
     }
 
     public FarmSystem getFarmSystem() {
-        return this.god;
+        return this.game;
     }
 
     public void advanceDay(MyFarm farm) {
-        god.addDay();
-        farm.addDay();
+        game.addDay();
+        farm.ageLot();
     }
 
+    public int getObjectCoins() {
+        return this.objectCoins;
+    }
+
+    public FarmerType getType() {
+        return this.type;
+    }
+
+    public int getLevel() {
+        return this.level;
+    }
 }
