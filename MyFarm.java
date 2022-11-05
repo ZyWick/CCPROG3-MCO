26affb728271f2cd5702883899128b39c8c1be57
Joshua Corpuz <104405536+ZyWick@users.noreply.github.com>
2022-11-05 12:04:18 +0800

    Update MyFarm.java

--- MyFarm.java
+++ MyFarm.java
@@ -13,61 +13,6 @@ public class MyFarm {
                 //lot.add(new Tile(true));
             
     }
-
-    public char whatToPrint(Tile TheTile) {
-
-        if (TheTile.isPlowed()) {
-            if (TheTile.getSeeds() != null) {
-                int day = TheTile.getDay();
-                int harvestTime = TheTile.getSeeds().getHarvestTime();
-    
-                if (day <= 1)
-                    return ',';
-                if (day <= harvestTime/3)
-                    return 's';
-                if (day <= harvestTime/1.5)
-                    return 'S'; 
-                if (day == harvestTime)
-                    return '$'; 
-                if (TheTile.isWithered() != 0 || day > harvestTime)
-                return 'X';        
-            }
-            return '#';
-        } else if (TheTile.isRock()) {
-            return '*';
-        } else {
-            return '=';
-        }
-    }
-
-    public void display () {
-        int x, y, row = 1, column = 1, tileIndex = 0;
-
-        for(x = 0; x < row; x++) {
-            for(y = 0; y < column; y++) 
-                System.out.print("* - - - - - ");
-            System.out.println('*');
-
-            for(y = 0; y < column; y++)
-                System.out.print("|           ");
-            System.out.println('|');
-                
-            for(y = 0; y < column; y++) {
-                System.out.print("|     ");
-                System.out.print(whatToPrint(lot.get(tileIndex)));
-                System.out.print("     ");
-                tileIndex = tileIndex + 1;
-            }
-            System.out.println('|');
-
-            for(y = 0; y < column; y++)
-                System.out.print("|           ");
-            System.out.println('|');
-        }        
-        for(y = 0; y < column; y++) 
-            System.out.print("* - - - - - ");
-        System.out.println('*');
-    }
     
     public void ageLot() {    
         game.addDay();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
