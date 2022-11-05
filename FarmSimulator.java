e24773e36a3f82764694e678dd60615ddfd7a38c
Joshua Corpuz <104405536+ZyWick@users.noreply.github.com>
2022-11-05 12:08:09 +0800

    Update FarmSimulator.java

--- FarmSimulator.java
+++ FarmSimulator.java
@@ -5,43 +5,28 @@ public class FarmSimulator {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         MyFarm farm = new MyFarm();
-        Player p1 = new Player(farm.getGame().getType().get(0));
         FarmSystem game = farm.getGame();
-        int choice, tileIndex, x = 1;
-        Tile TheTile = null;
-        farm.display();
+        Player p1 = new Player(farm);
+        int choice, x = 1;
 
+        game.display(farm.getLot());
         while (x != 0) {
 
-            p1.displayPlayerStats(farm);
+            p1.displayPlayerStats();
             game.displayGameMoves();
             System.out.print("Choice: ");
             choice = sc.nextInt();
 
             switch (choice) {
-                case 1: farm.display(); 
+                case 1: game.display(farm.getLot()); 
                         break;
-                case 2: tileIndex = game.getTileIndex(sc);
-                        TheTile = farm.getLot().get(tileIndex);
-                        game.displayInteractionChoices ();
-                        choice = sc.nextInt();
-                        switch (choice) {
-                            case 1: choice = game.getToolChoice(sc, TheTile, p1.getObjectCoins());
-                                    FarmTools selectedTool = game.getTools().get(choice);
-                                    p1.useTool(farm, selectedTool, TheTile);
-                                    break;
-                            case 2: p1.plantCrop(farm, TheTile, sc);
-                                    break;
-                            case 3: p1.harvestCrop(farm, TheTile); 
-                                    break;
-                            default: break;
-                        }
+                case 2: p1.interactTile(sc);
                         break;
-                case 3: p1.advanceDay(farm); 
+                case 3: p1.advanceDay(); 
                         System.out.println("Day: " + game.getDay()); 
                         break;
                 case 4: break;
-                case 5: p1.RegisterUp(farm);
+                case 5: p1.RegisterUp();
                 default: break;
             }
         }
