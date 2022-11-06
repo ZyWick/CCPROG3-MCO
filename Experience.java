12335e2adecbdf2af8dc8f0b430660006b9fca79
Harvey Chua <21309311+3liteking148@users.noreply.github.com>
2022-11-06 14:50:57 +0800

    JavaDoc Experience.java

--- Experience.java
+++ Experience.java
@@ -5,16 +5,28 @@ public class Experience {
     public Experience() {
     }
 
-
-    public void addExp(double d) {
-        exp += d;
+    /**
+     * Adds experience to the experience counter
+     * @param exp number of experience points to add
+     */
+    public void addExp(double exp) {
+        exp += exp;
         level = (int)(exp / 100);
     }
 
+    /**
+     * Returns the number of experience points
+     * @return the number of experience points
+     */
     public double getExp() {
         return exp;
     }
 
+    /**
+     * Returns the level.
+     * The level is calculated from the number of experience points with the formula level = floor(exp / 100)
+     * @return the level
+     */
     public int getLevel() {
         return level;
     }
