0fc29a32d1db2ecdeb4da5ceaee0a9f1ad9018a8
Harvey Chua <21309311+3liteking148@users.noreply.github.com>
2022-11-06 20:17:08 +0800

    More JavaDoc

--- Experience.java
+++ Experience.java
@@ -11,6 +11,7 @@ public class Experience {
 
     /**
      * Adds experience to the experience counter
+     *
      * @param expToAdd number of experience points to add
      */
     public void addExp(double expToAdd) {
@@ -20,6 +21,7 @@ public class Experience {
 
     /**
      * Returns the number of experience points
+     *
      * @return the number of experience points
      */
     public double getExp() {
@@ -29,6 +31,7 @@ public class Experience {
     /**
      * Returns the level.
      * The level is calculated from the number of experience points with the formula level = floor(exp / 100)
+     *
      * @return the level
      */
     public int getLevel() {
--- FarmSeeds.java
+++ FarmSeeds.java
@@ -1,3 +1,6 @@
+/**
+ * The type Farm seeds.
+ */
 public class FarmSeeds {
     private String name;
     private String cropType;
@@ -12,6 +15,22 @@ public class FarmSeeds {
     private int sellingPrice;
     private double expYield;
 
+    /**
+     * Instantiates a new Farm seeds.
+     *
+     * @param name                the name
+     * @param cropType            the crop type
+     * @param harvestTime         the harvest time
+     * @param waterNeeds          the water needs
+     * @param waterLimit          the water limit
+     * @param fertilizerNeeds     the fertilizer needs
+     * @param fertilizerLimit     the fertilizer limit
+     * @param minProductsProduced the min products produced
+     * @param maxProductsProduced the max products produced
+     * @param seedCost            the seed cost
+     * @param sellingPrice        the selling price
+     * @param expYield            the exp yield
+     */
     public FarmSeeds(String name, String cropType, int harvestTime, int waterNeeds, int waterLimit, int fertilizerNeeds, int fertilizerLimit, int minProductsProduced, int maxProductsProduced, int seedCost, int sellingPrice, double expYield) {
         this.name = name;
         this.cropType = cropType;
@@ -26,51 +45,111 @@ public class FarmSeeds {
         this.sellingPrice = sellingPrice;
         this.expYield = expYield;
     }
-    
+
+    /**
+     * Gets the name of the crop.
+     *
+     * @return the name of the crop
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Gets crop type.
+     *
+     * @return the crop type
+     */
     public String getCropType() {
         return this.cropType;
     }
 
+    /**
+     * Gets harvest time.
+     *
+     * @return the harvest time
+     */
     public int getHarvestTime() {
         return this.harvestTime;
     }
 
+    /**
+     * Gets water needs.
+     *
+     * @return the water needs
+     */
     public int getWaterNeeds() {
         return this.waterNeeds;
     }
 
+    /**
+     * Gets water limit.
+     *
+     * @return the water limit
+     */
     public int getWaterLimit() {
         return this.waterLimit;
     }
 
+    /**
+     * Gets fertilizer needs.
+     *
+     * @return the fertilizer needs
+     */
     public int getFertilizerNeeds() {
         return this.fertilizerNeeds;
     }
 
+    /**
+     * Gets fertilizer limit.
+     *
+     * @return the fertilizer limit
+     */
     public int getFertilizerLimit() {
         return this.fertilizerLimit;
     }
-    
+
+    /**
+     * Gets min products produced.
+     *
+     * @return the min products produced
+     */
     public int getMinProductsProduced() {
         return this.minProductsProduced;
     }
 
+    /**
+     * Gets max products produced.
+     *
+     * @return the max products produced
+     */
     public int getMaxProductsProduced() {
         return this.maxProductsProduced;
     }
 
+    /**
+     * Gets seed cost.
+     *
+     * @return the seed cost
+     */
     public int getSeedCost() {
         return this.seedCost;
     }
 
+    /**
+     * Gets selling price.
+     *
+     * @return the selling price
+     */
     public int getSellingPrice() {
         return this.sellingPrice;
     }
 
+    /**
+     * Gets the experience yield.
+     *
+     * @return the experience yield
+     */
     public double getExpYield() {
         return this.expYield;
     }
