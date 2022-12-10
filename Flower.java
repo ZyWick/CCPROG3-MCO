baaa41a36d94bf1595b51e1c0f9aacaede2813ef
Joshua Corpuz <104405536+ZyWick@users.noreply.github.com>
2022-12-10 16:20:08 +0800

    document for FarmSeeds subclass

--- Flower.java
+++ Flower.java
@@ -1,9 +1,27 @@
 public class Flower extends FarmSeeds{
     
+    /** Creates a Flower object
+     * 
+     * @param name                the name
+     * @param cropType            the crop type
+     * @param harvestTime         the harvest time
+     * @param waterNeeds          the minimum amount water needed
+     * @param waterLimit          the water bonus limit
+     * @param fertilizerNeeds     the minimum amount of fertilizer needed
+     * @param fertilizerLimit     the fertilizer bonus limit
+     * @param minProductsProduced the minimum amount of products produced
+     * @param maxProductsProduced the maximum amount of products produced
+     * @param seedCost            the seed cost
+     * @param sellingPrice        the selling price
+     * @param expYield            the experience gained by harvesting
+     */
     public Flower(String name, /*String cropType,*/ int harvestTime, int waterNeeds, int waterLimit, int fertilizerNeeds, int fertilizerLimit, int minProductsProduced, int maxProductsProduced, int seedCost, int sellingPrice, double expYield) {
         super(name, "Flower", harvestTime, waterNeeds, waterLimit, fertilizerNeeds, fertilizerLimit, minProductsProduced, maxProductsProduced, seedCost, sellingPrice, expYield);
     }
 
+    /** gets the premium factor of the flower crop
+     * @return a double multiplier to the harvest objectCoin yield as bonus for being pretty
+     */
     public double getPremiumFactor() {
         return 1.1;
     }
