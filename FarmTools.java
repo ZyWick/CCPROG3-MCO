7564865f1a8ce55c38c73b589cd31763e3eeea74
Joshua Corpuz <104405536+ZyWick@users.noreply.github.com>
2022-12-10 15:08:00 +0800

    document FarmTool

--- FarmTools.java
+++ FarmTools.java
@@ -3,41 +3,34 @@
  */
 public interface FarmTools {
 
-    // /**
-    //  * Creates a farm tool
-    //  * @param name name of the tool
-    //  * @param usageCost cost of using the tool
-    //  * @param expYield amount of experience gained from using the tool
-    //  */
-    // public FarmTools(String name, int usageCost, double expYield) {
-    //     this.name = name;
-    //     this.usageCost = usageCost;
-    //     this.expYield = expYield;
-    // }
+    /** tests if a certain tool can be used on a tile of a farm
+     * 
+     * @param farm the farm to which you will use the tool
+     * @param coordinates the coordinates of the tile
+     * @return int determining the error that occurred or none
+     */
     public int canUseTool(MyFarm farm, Coordinates coordinates);
-    // /**
-    //  * Returns the name of the tool
-    //  * @return name of the tool
-    //  */
+
+    /** applies the function of the tool to a tile on the farm based on the specifications
+     * 
+     * @param farm the farm to which you will use the tool
+     * @param coordinates the coordinates of the tile
+     * @return a double array containing the yield of using the tool
+     */
+    public double[] useTool(MyFarm farm, Coordinates coordinates);
+ 
+    /** gets the yield from using a tool
+     * @return an array containing {cost of using the tool, amount of experience gained}
+     */
+    public double[] getToolCostAndYield();
+   
+    /** gets the name of the tool
+     * @return name of the tool
+     */
     public String getName();
 
-    // /**
-    //  * Returns the cost of using the tool
-    //  * @return cost of using the tool
-    //  */
+    /** gets usage cost
+     * @return cost of using the tool
+     */
     public int getUsageCost();
-
-    // /**
-    //  * Returns the amount of experience gained from using the tool
-    //  * @return amount of experience gained
-    //  */
-    // public double getExpYield();
-
-    // /**
-    //  * Returns the cost of using the tool and amount of experience gained from using the tool
-    //  * @return an array containing {cost of using the tool, amount of experience gained}
-    //  */
-    public double[] getToolCostAndYield();
-
-        public double[] useTool(MyFarm farm, Coordinates tileIndex);
 }
